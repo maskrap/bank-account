0bbfe565bd2a03966b2c199c4f8e8578914780fd
Cory Olson and Sam Park <sam@>
2016-06-14 15:14:58 -0700

    We got a history log baby

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 function BankAccount(name, deposit) {
   this.name = name;
   this.balance = parseInt(deposit);
+  this.history = [];
+  this.history.push("initial deposit" + deposit);
 };
 
 BankAccount.prototype.deposit = function(deposit) {
@@ -18,6 +20,7 @@ var user = Object;
 $(function()  {
   $("#open-account").submit(function(event)  {
     event.preventDefault();
+    $(".history").text("");
     user = new BankAccount($("#name").val(), $("#initial-deposit").val());
     $(".balance").text("");
     $(".balance").append("<h2>Balance $" + parseInt(user.balance).toFixed(2) + "</h2>");
@@ -27,11 +30,13 @@ $(function()  {
     var balance = user.deposit($("#funds").val());
     $(".balance").text("");
     $(".balance").append("<h2>Balance $" + parseInt(balance).toFixed(2) + "</h2>");
+    $(".history").append("<h2>" + Date() + "Deposits" + parseInt(balance).toFixed(2) + "</h2>");
   });
 
   $("#withdrawal").click(function(event)  {
    var balance = user.withdrawal($("#funds").val());
    $(".balance").text("");
    $(".balance").append('<h2>Balance $' + parseInt(balance).toFixed(2) + '</h2>');
+   $(".history").append("<h2>" + Date() + "Withdrawals $" + parseInt(balance).toFixed(2) + "</h2>");
   });
 });
